891d4c102dce7dbfcb0290f2a959b6ac0523bc7d
JamesBremner <james@ravenspoint.com>
2021-12-14 15:41:47 -0500

    document maze class interface

--- src/cMaze.h
+++ src/cMaze.h
@@ -2,17 +2,28 @@ namespace raven
 {
     namespace graph
     {
+        // forward definition of class finding paths through graphs
         class cPathFinder;
 
+        /// represents a 2D grid maze
         class cMaze
         {
         public:
+            /// read ascii art maze specification
             void read(std::ifstream &file);
+
+            /** convert maze to a graph with nodes and links
+             * @param[out] finder  the graph
+             */
             void graph(cPathFinder &finder);
+
+            /// get number of rows in maze grid
             int rowCount() const
             {
                 return (int)myMaze.size();
             }
+
+            /// get number of columns in maze grid
             int colCount() const
             {
                 if (!rowCount())
@@ -21,6 +32,8 @@ namespace raven
             }
 
         private:
+
+            /// represents a single cell in a maze
             class cCell
             {
             public:
@@ -32,8 +45,12 @@ namespace raven
                 {
                 }
             };
+
+            /// the maze represented as a 2D vector of calls
             std::vector<std::vector<cCell>> myMaze;
-            int myStart, myEnd;         // 0-based indices of start and end cells
+
+            /// 0-based indices of start and end cells
+            int myStart, myEnd;        
         };
     }
 }
